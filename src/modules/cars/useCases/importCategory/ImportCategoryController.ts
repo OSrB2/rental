566a6a7226841e9ed058bdfd0041ebb6b783be29
Pedro <pedro_oliva@oulook.com.br>
2023-02-10 18:49:54 -0300

    feat: config fs and csv-parse, upload file

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -3,6 +3,7 @@ import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+
   handle(req: Request, res: Response): Response {
     const { file } = req;
 
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,16 @@
-
+import { parse as csvParse } from 'csv-parse';
+import fs from 'fs';
 
 class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse();
 
-  execute(file: any) {
-    console.log(file)
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
 
